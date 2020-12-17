24d0c44baae5293b844a567b8df6b4cc4d2c8610
Quentin Pongratz <lagarathan@gmail.com>
2020-12-17 11:06:20 -0600

    Add functions for changing background color on hover

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let gridSize = 16;
+let selectedColor = "black";
 const sketchArea = document.querySelector('.sketch-area');
 
 for (let i=0; i<(gridSize**2); i++) {
@@ -6,4 +7,15 @@ for (let i=0; i<(gridSize**2); i++) {
     pixel.className = 'pixel';
     sketchArea.appendChild(pixel);
 }
+setHover();
 
+function setHover(){
+    let pixels = document.querySelectorAll('.pixel');
+    pixels.forEach(pixel => {
+        pixel.addEventListener('mouseover', changeColor);
+    });
+}
+
+function changeColor(event){
+    event.target.style.backgroundColor = selectedColor;    
+}
